79eebab93e5ac6d8f4dc9571caae943f3c96f7d9
wjl7123093 <wjl7123093@163.com>
2018-01-01 10:02:01 +0800

    SharePreference Keys

---
+++ app/src/main/java/com/fred_w/demo/codercommunity/app/SharepreferenceKey.java
@@ -0,0 +1,17 @@
+package com.fred_w.demo.codercommunity.app;
+
+/**
+ * Sharepreference Keys
+ *
+ * @author Fred_W
+ * @version v1.0.0
+ *
+ * @crdate 2018-1-1
+ * @update
+ */
+public interface SharepreferenceKey {
+
+    // Access_Token
+    String KEY_ACCESS_TOKEN = "access_token";
+
+}

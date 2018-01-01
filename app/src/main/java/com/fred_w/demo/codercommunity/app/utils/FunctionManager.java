ef5228c71bdc35c5285a55cc1f50b26635efed39
wjl7123093 <wjl7123093@163.com>
2018-01-01 21:54:25 +0800

    xml 解析类(配合 ListView/GridView 使用)

---
+++ app/src/main/java/com/fred_w/demo/codercommunity/app/utils/FunctionManager.java
@@ -0,0 +1,72 @@
+package com.fred_w.demo.codercommunity.app.utils;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.fred_w.demo.codercommunity.mvp.model.entity.LvMineFunctionBean;
+
+import java.util.List;
+
+/**
+ * FunctionManager xml功能管理类
+ *
+ * @author Fred_W
+ * @version v1.0.0
+ *
+ * @crdate 2018-1-1
+ * @update
+ */
+public class FunctionManager {
+
+	private static volatile FunctionManager instance = null;
+	private Context mContext;
+	private FunctionXMLReader<LvMineFunctionBean> mXmlReader;
+	private List<LvMineFunctionBean> mBeans;
+	
+	private FunctionManager(Context context) {
+		this.mContext = context;
+	}
+	
+	/** 单例 */
+	public static FunctionManager getInstance(Context context) {
+		if (null == instance) {
+			synchronized (FunctionManager.class) {
+				if (null == instance) {
+					instance = new FunctionManager(context);
+				}
+			}
+		}
+		return instance;
+	}
+	
+	/** 初始化 */
+	public void init() {
+		mXmlReader = new FunctionXMLReader<LvMineFunctionBean>(
+				mContext, LvMineFunctionBean.class);
+		mXmlReader.parse();
+		mBeans = mXmlReader.getBeans();
+	}
+	
+	/** 得到 GvFunctionBean */
+	public List<LvMineFunctionBean> getBeans() {
+		return mBeans;
+	}
+	
+	/** 
+	 * 执行每一项功能
+	 * @param position 项索引
+	 * @throws ClassNotFoundException
+	 */
+	public void lanchFunction(int position)
+			throws ClassNotFoundException {
+		LvMineFunctionBean bean = mBeans.get(position);
+		String name = bean.getName();	// 功能名称
+		String className = bean.getPackageName() + "." + bean.getActivityName();
+
+		Intent intent = new Intent();
+		intent.setClass(mContext, Class.forName(className));
+		mContext.startActivity(intent);
+
+	}
+	
+}

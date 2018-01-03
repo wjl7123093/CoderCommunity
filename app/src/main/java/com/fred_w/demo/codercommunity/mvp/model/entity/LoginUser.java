3fa1ccd7a225a54e74259a7e34ef3f1445c32d26
wjl7123093 <wjl7123093@163.com>
2018-01-03 20:58:20 +0800

    登录用户 Entity

---
+++ app/src/main/java/com/fred_w/demo/codercommunity/mvp/model/entity/LoginUser.java
@@ -0,0 +1,90 @@
+package com.fred_w.demo.codercommunity.mvp.model.entity;
+
+/**
+ * 登录用户 Entity
+ *
+ * @author Fred_W
+ * @version v1.0.0
+ *
+ * @crdate 2017-1-2
+ * @update
+ */
+public class LoginUser {
+
+    private String id;
+    private String email;
+    private String name;
+    private String gender;
+    private String avatar;
+    private String location;
+    private String url;
+
+    @Override
+    public String toString() {
+        return "LoginUser{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", location='" + location + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}

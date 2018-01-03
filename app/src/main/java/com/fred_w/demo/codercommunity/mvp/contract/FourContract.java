74e70bc18b4bfc15755ebf76637c676ee180f346
wjl7123093 <wjl7123093@163.com>
2018-01-03 20:56:50 +0800

    个人中心 VM 接口

--- app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/FourContract.java
+++ app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/FourContract.java
@@ -1,17 +1,28 @@
 package com.fred_w.demo.codercommunity.mvp.contract;
 
+import com.fred_w.demo.codercommunity.mvp.model.entity.LoginUser;
 import com.jess.arms.mvp.IView;
 import com.jess.arms.mvp.IModel;
+
+import io.reactivex.Observable;
 
 
 public interface FourContract {
     //对于经常使用的关于UI的方法可以定义到IView中,如显示隐藏进度条,和显示文字消息
     interface View extends IView {
-
+        void saveLoginUser(LoginUser loginUser);
+        void showHeaderImage(String imgUrl);
+        void showName(String name);
     }
 
     //Model层定义接口,外部只需关心Model返回的数据,无需关心内部细节,即是否使用缓存
     interface Model extends IModel {
-
+        /**
+         * 获取当前登录用户的账户信息
+         * @param access_token oauth2_token获取的access_token
+         * @param dataType 返回数据类型['json'|'jsonp'|'xml']
+         * @return
+         */
+        Observable<LoginUser> getLoginUser(String access_token, String dataType);
     }
 }

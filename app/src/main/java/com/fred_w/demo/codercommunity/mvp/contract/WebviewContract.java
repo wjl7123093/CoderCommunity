6b27692f822badcebfa967e006648ced76967111
wjl7123093 <wjl7123093@163.com>
2018-01-03 20:57:15 +0800

    登录认证 VM 接口

--- app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/WebviewContract.java
+++ app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/WebviewContract.java
@@ -20,6 +20,13 @@ public interface WebviewContract {
     interface Model extends IModel {
         /**
          * 获取 AccessToken
+         * @param client_id OAuth2客户ID
+         * @param client_secret OAuth2密钥
+         * @param grant_type 授权方式：authorization_code或者refresh_token
+         * @param redirect_uri 回调地址
+         * @param code 调用 /action/oauth2/authorize 接口返回的授权码(grant_type为authorization_code时必选)
+         * @param dataType 返回数据类型['json'|'jsonp'|'xml']
+         * @return
          */
         Observable<AccessToken> getAccessToken(String client_id, String client_secret,
                                                          String grant_type, String redirect_uri,

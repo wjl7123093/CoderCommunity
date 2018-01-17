0329e5480435904328fb24015961f9f2bb4bde68
wjl <wjl7123093@163.com>
2018-01-17 11:42:39 +0800

    新增 finishRefreshing() 接口

--- app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/ActiveListContract.java
+++ app/src/main/java/com/fred_w/demo/codercommunity/mvp/contract/ActiveListContract.java
@@ -10,11 +10,20 @@ import java.util.List;
 
 import io.reactivex.Observable;
 
-
+/**
+ * ActiveListContract IV & IM 接口
+ *
+ * @author Fred_W
+ * @version v1.0.0
+ *
+ * @crdate 2018-1-16
+ * @update 2017-1-17 新增 finishRefreshing() 接口
+ */
 public interface ActiveListContract {
     //对于经常使用的关于UI的方法可以定义到IView中,如显示隐藏进度条,和显示文字消息
     interface View extends IView {
         void showActiveList(List<Active> activeList);
+        void finishRefreshing();
     }
 
     //Model层定义接口,外部只需关心Model返回的数据,无需关心内部细节,即是否使用缓存
